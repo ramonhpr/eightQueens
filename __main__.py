fcace2c7cec4ed8b2f63e6bbddf654fa5809f754
Ramon Henrique <rhpr@cin.ufpe.br>
2018-03-24 23:44:00 -0300

    Add initial source code to show population

---
+++ __main__.py
@@ -0,0 +1,18 @@
+from util.Population import Population
+import config
+
+def cutAndCross():
+	pass
+
+pop = Population(config.NUM_INDIVIDUALS)
+pop.setCrossoverFunct(cutAndCross)
+# Show children created
+for ind in pop.individuals:
+	print(ind.genotype)
+
+for i in range(0, config.ITERATIONS_LEN):
+	# TODO: There should be a funtion like 'run'
+	# 		to allow to select children and the survivors
+	# 		and the crossover
+	pass
+# TODO: Plot a graph with the solution
