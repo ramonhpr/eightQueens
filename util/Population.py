1bf6492a7995950fd950c2f0a2f2430d204d3870
Ramon Henrique <rhpr@cin.ufpe.br>
2018-03-27 07:54:30 -0300

    Allow to set the fitness function to all population

--- util/Population.py
+++ util/Population.py
@@ -6,8 +6,20 @@ class Population:
 		string individuals,the default bits is 5
 		"""
 		self.individuals = []
+		self.epoch = 0
 		for i in range(0, num_individuals):
 			self.individuals.append(Individual(numBits))
+
+	def setFitnessFunc(self, fitnessFunc):
+		"""
+		Set for all individuals in the population the fitness function passed
+		If the population is "running" it raise an exception
+		"""
+		if self.epoch > 0:
+			raise Exception('You cannot pass a function when the population is running')
+		else:
+			for ind in self.individuals:
+				ind.setFitnessFunc(fitnessFunc)
 
 	def setCrossoverFunct(self, func):
 		"""
