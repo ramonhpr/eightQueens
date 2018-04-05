d4367279d13c037846b435328d6c6bdb4e8d1f57
Ramon Henrique <rhpr@cin.ufpe.br>
2018-04-04 22:13:31 -0300

    Fix incorrect access to function randint

--- util/Individual.py
+++ util/Individual.py
@@ -111,11 +111,11 @@ class Individual:
 		#positions to be found
 		pos1, pos2 = 0, 0
 		# compute chances of mutation
-		if(random.randint(1,10) < (MUTATION_PROB * 10)):
+		if(randint(1,10) < (MUTATION_PROB * 10)):
 			# pickup two different numbers to mutate in genotype
 			while(pos1==pos2):
-				pos1 = random.randint(0,7)
-				pos2 = random.randint(0,7) 
+				pos1 = randint(0,7)
+				pos2 = randint(0,7)
 			# swap the first and second gene in genotype
 			self.genotype[pos1],self.genotype[pos2]=self.genotype[pos2],self.genotype[pos1]
 		
